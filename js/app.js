386fe26721560626d792790e638eb6a5c9bf60db
brubakerjeff <brubaker.jeff@gmail.com>
2014-11-01 23:59:15 -0400

    try again

--- js/app.js
+++ js/app.js
@@ -51,10 +51,10 @@ myApp.controller('MyCtrl', ['$scope', function($scope) {
 	$scope.VisiblePage=1;
 //$scope.expression ="4";
     $scope.$watchCollection('[Pressure,Length,Modulus,Radius,Thickness,VisiblePage]', function (value) {
-        $scope.expressions = "\\sigma_{cr}=\\dfrac{\\pi^2 E t^2}{12(1-\\nu^2)L^2} = \\ " + 
+        $scope.expressions = "\\sigma_{cr}=\\dfrac{\\pi^2 E t^2}{12(1-\\nu^2)L^2} = \\\\ " + 
 		String(((Math.pow(3.14159,2)*$scope.Modulus*Math.pow(10,6)*Math.pow($scope.Thickness,2))/(12*(1-$scope.Poisson)*Math.pow($scope.Length,2))).toFixed(2))
               + "\\text{ psi}";
-		$scope.expressionp = "p_{cr}=\\dfrac{Et^3}{12R^3(1-\\nu^2)} = \\" + 
+		$scope.expressionp = "p_{cr}=\\dfrac{Et^3}{12R^3(1-\\nu^2)} = \\\\" + 
 		String((($scope.Modulus*Math.pow(10,6)*Math.pow($scope.Thickness,3))/(12*Math.pow($scope.Radius,3)*(1-Math.pow($scope.Poisson,2)))).toFixed(2)) 
         + "\\text{ psi}";
 
